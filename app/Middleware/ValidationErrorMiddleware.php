e42f059c372a3fa322ed4e58ad7ea688f892d313
Szymon <ZawiszaCzarny52@gmail.com>
2017-07-03 08:13:54 +0200

    'app'

--- app/Middleware/ValidationErrorMiddleware.php
+++ app/Middleware/ValidationErrorMiddleware.php
@@ -9,11 +9,13 @@ class ValidationErrorMiddleware extends Middleware {
     public function __invoke($request, $response, $next)
     {
 
-$this->container->view->getEnvironment()->addGlobal('errors', $_SESSION['errors']);
+        if (isset($_SESSION['errors'])) {
 
-unset($_SESSION['errors']);
+            $this->container->view->getEnvironment()->addGlobal('errors', $_SESSION['errors']);
 
-//
+            unset($_SESSION['errors']);
+
+        }
        $response = $next($request, $response);
        return $response;
     }
